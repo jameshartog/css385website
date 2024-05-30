befc82b96a9304c46a7e997c2c65512a70c4ff6e
JJ Hartog <jjhartog@uw.edu>
2024-05-30 13:13:05 -0700

    Third Round Implementation

--- src/routes/Documents.js
+++ src/routes/Documents.js
@@ -9,7 +9,7 @@ const Documents = () => {
             <h1 className={styles.Paragraph}>
                 <a href="https://docs.google.com/document/d/11TMmwmjaexXqAxAKdUquSGzieaoYoRcBkO9boA7IFVY/edit">Playtesting reports</a><br></br>
                 <a href="https://docs.google.com/presentation/d/1U2zkSV2nDC1JP-TbS_zPJwfv5MmySwf_NmOUhaPV8L4/edit?usp=sharing">Postmortem Presentation</a><br></br>
-                <a href="https://docs.google.com/presentation/d/1VssOT86Ikx1DNmzjEZCNR-DNagDRZXnBTbTeSGE0BpA/edit?usp=sharing">Presentation</a><br></br>
+                <a href="https://docs.google.com/presentation/d/1VssOT86Ikx1DNmzjEZCNR-DNagDRZXnBTbTeSGE0BpA/edit?usp=sharing">Game Presentation</a><br></br>
                 <a href="https://docs.google.com/document/d/1LKimrAUhkm7g_SLv3fV9P9x_2-lOAIBKP88lXF0Td00/edit?usp=sharing">Team Reflection</a><br></br>
             </h1>
         </div>
