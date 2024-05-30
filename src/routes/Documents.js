bd05a03e259ac2c5f50f3a030c7d7cd054d53555
JJ Hartog <jjhartog@uw.edu>
2024-05-30 01:16:37 -0700

    Second Round Implementation

--- src/routes/Documents.js
+++ src/routes/Documents.js
@@ -10,7 +10,7 @@ const Documents = () => {
                 <a href="https://github.com/DillonJEvans/css385-final">Playtesting reports</a><br></br>
                 <a href="https://docs.google.com/presentation/d/1U2zkSV2nDC1JP-TbS_zPJwfv5MmySwf_NmOUhaPV8L4/edit?usp=sharing">Postmortem Presentation</a><br></br>
                 <a href="https://docs.google.com/presentation/d/1VssOT86Ikx1DNmzjEZCNR-DNagDRZXnBTbTeSGE0BpA/edit?usp=sharing">Presentation</a><br></br>
-                <a href="https://github.com/DillonJEvans/css385-final">Team Reflection</a><br></br>
+                <a href="https://docs.google.com/document/d/1LKimrAUhkm7g_SLv3fV9P9x_2-lOAIBKP88lXF0Td00/edit?usp=sharing">Team Reflection</a><br></br>
             </h1>
         </div>
     );
